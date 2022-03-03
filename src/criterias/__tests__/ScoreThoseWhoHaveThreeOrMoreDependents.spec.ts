fe1c9c4755c37e7b808ef4f6666d3727cda12a08
Bruno Henrique <bruno28dpvat@gmail.com>
2022-03-03 13:58:59 -0400

    feat: added test to criteria file ScoreThoseWhoHaveThreeOrMoreDependents

---
+++ src/criterias/__tests__/ScoreThoseWhoHaveThreeOrMoreDependents.spec.ts
@@ -0,0 +1,72 @@
+import { scoreThoseWhoHaveThreeOrMoreDependents } from '../'
+import { IFamily } from '../../interfaces';
+
+describe('ScoreThoseWhoHaveThreeOrMoreDependents', () => {
+  it('should be able to return three if the family has three or more dependents under the age of eighteen', async () => {
+    const family: IFamily = {
+      income: 900,
+      score: 0,
+      candidate: {
+        name: 'Maria',
+        age: 36,
+        score: 0,
+      },
+      candidatePartner: {
+        name: 'Pedro',
+        age: 40
+      },
+      dependents: [
+        {
+          name: 'Pedro',
+          age: 17
+        },
+        {
+          name: 'Jorge',
+          age: 14
+        },
+        {
+          name: 'Bia',
+          age: 17
+        },
+      ]
+    }
+
+    const score = scoreThoseWhoHaveThreeOrMoreDependents(family)
+
+    expect(score).toEqual(3);
+  })
+
+  it('must be able to return zero if the family has less than three dependents under the age of eighteen', async () => {
+    const family: IFamily = {
+      income: 900,
+      score: 0,
+      candidate: {
+        name: 'Maria',
+        age: 36,
+        score: 0,
+      },
+      candidatePartner: {
+        name: 'Pedro',
+        age: 40
+      },
+      dependents: [
+        {
+          name: 'Pedro',
+          age: 17
+        },
+        {
+          name: 'Jorge',
+          age: 14
+        },
+        {
+          name: 'Bia',
+          age: 18
+        },
+      ]
+    }
+
+    const score = scoreThoseWhoHaveThreeOrMoreDependents(family)
+
+    expect(score).toEqual(0);
+  })
+})
